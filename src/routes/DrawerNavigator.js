c096f901611c4bcca4a19951418a355104100f0f
Ian Salt <ian@isalt.digital>
2018-10-23 17:33:55 +0100

    custom drawer component created

--- src/routes/DrawerNavigator.js
+++ src/routes/DrawerNavigator.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import { createDrawerNavigator } from 'react-navigation';
-
+import {
+  Dimensions,
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { createDrawerNavigator, DrawerItems } from 'react-navigation';
 import { Home, Lists, Login, Users } from '../Screens';
 import ListScrollView from '../Components/ListScrollView';
 
-const DrawerNavigator = createDrawerNavigator(
+const DrawerNavigator = () => {
+  return (
+    <AppDrawerNavigator />
+  );
+};
+
+const tnq = require('../images/TNQ.jpg');
+
+const CustomDrawerComponent = (props) => {
+  return (
+    <ScrollView>
+      <SafeAreaView style={styles.safeAreaViewStyles}>
+        <View style={styles.headerStyle}>
+          <Image
+            style={styles.imageStyle}
+            source={tnq}
+          />
+          <Text>Drawer Header</Text>
+        </View>
+        <DrawerItems {...props} />
+      </SafeAreaView>
+    </ScrollView>
+  );
+};
+
+const AppDrawerNavigator = createDrawerNavigator(
   {
     Home,
     Lists,
@@ -13,8 +46,27 @@ const DrawerNavigator = createDrawerNavigator(
     ListScrollView,
   },
   {
+    contentComponent: CustomDrawerComponent,
     drawerBackgroundColor: '#cadbf7',
   }
 );
+
+const styles = StyleSheet.create({
+  headerStyle: {
+    backgroundColor: '#79a7f2',
+    height: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageStyle: {
+    borderRadius: 60,
+    flex: 1,
+    height: 120,
+    width: 120,
+  },
+  safeAreaViewStyles: {
+    flex: 1,
+  },
+});
 
 export default DrawerNavigator;
