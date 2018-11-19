230c14fcaa542b19fab17d0edb3fd401f7d41552
Ian Salt <ian@isalt.digital>
2018-11-19 17:30:34 +0000

    list more dynamic

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -3,7 +3,16 @@ import { View, Text, Button } from 'react-native';
 import screenStyles from './styles/screenStyles';
 
 const HomeScreen = (props) => {
+  const users = [
+    { name: 'Rick' },
+    { name: 'Morty' },
+    { name: 'Summer' },
+    { name: 'Beth' },
+    { name: 'Jerry' }
+  ];
+
   const { navigate } = props.navigation;
+
   return (
     <View style={screenStyles.viewStyle}>
       <Text style={screenStyles.textStyle}>Home Screen</Text>
@@ -15,6 +24,18 @@ const HomeScreen = (props) => {
         onPress={() => navigate('Login', { name: 'Ian Salt' })}
         title='Login'
       />
+      {
+        users.map((user, i) => {
+          console.log(i, user.name);
+          return (
+            <Button
+              key={i}
+              onPress={() => navigate('Lists', { list: user.name })}
+              title={`List of ${user.name}`}
+            />
+          );
+        })
+      }
     </View>
   );
 };
